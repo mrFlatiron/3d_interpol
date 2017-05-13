6589608d68f6acd540ad018a6e51c60b0f5628fc
None <mr.flatiron@mail.ru>
2017-05-13 15:57:05 +0300

    test funcs

---
+++ src/test_functions/test_functions.cpp
@@ -0,0 +1,9 @@
+#include "test_functions.h"
+#include <cmath>
+
+double func (const double x, const double y)
+{
+  (void)x;
+  (void)y;
+ return sin (x);
+}
---
+++ src/test_functions/test_functions.h
@@ -0,0 +1,6 @@
+#ifndef TEST_FUNCTIONS_H
+#define TEST_FUNCTIONS_H
+
+double func (const double x, const double y);
+
+#endif // TEST_FUNCTIONS_H
